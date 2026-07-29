94fac9acd10497bafb8812af963dd89df0eba259
kutt27 <satheesanamal6@gmail.com>
2026-07-29 17:20:25 +0530

    added exit command

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,28 @@ fn main() {
 
 fn run_loop() {
     let mut buf = String::new();
-    let allowed_commands: [&str; 2] = ["hello", "exit"];
+    let allowed_commands: [&str; 1] = ["hello"];
+    let mut exit = false;
 
-    loop{
+    while !exit {
         print!("$ ");
         io::stdout().flush().expect("failed to flush stdout");
+
         buf.clear();
         io::stdin().read_line(&mut buf).expect("failed to read line");
+        
+        let trimmed = buf.trim();
+        if trimmed.is_empty() {
+            continue; // ignore empty lines
+        }
+
         let args: Vec<&str> = buf.trim().split_whitespace().collect();
+        
         if allowed_commands.contains(&args[0]) {
             print!("{}", buf);
+        } else if args[0] == "exit" {
+            println!("exited with {}", 0);
+            exit = true;
         } else {
             println!("{}: command not found", args[0]);
         }
